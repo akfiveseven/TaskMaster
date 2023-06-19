b92dbbfe6ad808ad2be54c7a6b7f485b2c0b6be4
aqzx127 <aqzx127@gmail.com>
2023-06-18 17:03:39 -0700

    v0.3

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import TaskList from './TaskList';
 import './style.css';
 
 export default function ToDoApp() {
@@ -7,14 +8,13 @@ export default function ToDoApp() {
     const [taskDueDate, setTaskDueDate] = useState('');
     const [newTaskData, setNewTaskData] = useState([]);
     const [showTasks, setShowTasks] = useState(false);
+
+    const [selectedOption, setSelectedOption] = useState('')
+
   
     function handleClick() {
       const newTask = { taskName, taskPriority, taskDueDate };
       setNewTaskData(prevData => [...prevData, newTask]);
-    }
-  
-    function handleShowTasks() {
-      setShowTasks(true);
     }
   
     const handleChange = e => {
@@ -28,6 +28,74 @@ export default function ToDoApp() {
     const handleDueDate = e => {
       setTaskDueDate(e.target.value);
     };
+
+    const handleSort = (e) => {
+        let newOption = e.target.value;
+        setSelectedOption(newOption);
+    }
+
+
+    
+    function sortByPriority() {
+        let temp = [...newTaskData]; // Create a new copy of the array
+      
+        const sortAlgo = (a, b) => {
+          const priorityOrder = ["High", "Medium", "Low"];
+          return priorityOrder.indexOf(a.taskPriority) - priorityOrder.indexOf(b.taskPriority);
+        };
+      
+        temp.sort(sortAlgo); // Sort the tasks based on priority level
+      
+        setNewTaskData(temp); // Update the state with the sorted tasks
+    }
+
+    function sortByDate() {
+        let temp = [...newTaskData];
+
+
+        // if (temp[0].taskDueDate < temp[1].taskDueDate) {
+        //   console.log("first is earlier date");
+        // }
+
+        const sortDate = (a, b) => {
+            if (a.taskDueDate < b.taskDueDate) {
+              return -1; // No change, date is in correct spot
+            }
+            else {
+              return 1; // Swaps the values to put them in the correct spot
+            } 
+        }
+
+        temp.sort(sortDate);
+        setNewTaskData(temp); // Update the state with the sorted tasks
+    }
+      
+
+
+
+
+
+    // WHENEVER SORT BY VALUE CHANGES, THIS USE EFFECT IS EXECUTED
+    useEffect(() => {
+        //console.log(selectedOption);
+        if (selectedOption === "priority") {
+            sortByPriority();
+        }
+        else if (selectedOption === "due-date") {
+          sortByDate();
+        }
+        else if (selectedOption === "sort") {
+          setNewTaskData([]);
+        }
+        
+
+        // TESTING
+        // let g = [1, 3, 5, 4, 2];
+        // selectionSort(g, g.length);
+        // printArr(g);
+
+
+    }, [selectedOption]);
   
     return (
       <div>
@@ -48,22 +116,55 @@ export default function ToDoApp() {
         </div>
   
         <div>
-          <button onClick={handleShowTasks}>Show Tasks</button>
-          {showTasks && (
+          {/* <button onClick={handleShowTasks}>Show Tasks</button> */}
             <>
               <h3>Task Data:</h3>
-              {newTaskData.map((task, index) => (
-                <div key={index}>
-                  <p>Task Name: {task.taskName}</p>
-                  <p>Task Priority: {task.taskPriority}</p>
-                  <p>Task Due Date: {task.taskDueDate}</p>
-                  <hr />
-                </div>
-              ))}
+              <select name="sort" id="Sort" onChange={handleSort}>
+                <option value="sort">Sort By</option>
+                <option value="priority">Priority</option>
+                <option value="due-date">Due Date</option>
+              </select>
+              <TaskList tasks={newTaskData} />
             </>
-          )}
         </div>
       </div>
     );
   }
   
+    // function selectionSort(tempArray, size) {
+    //   // THIS SELECTION SORT ONLY SORTS THE TASKS BY PRIORITY
+    //   var i, j, min_index;
+
+    //   for (let i = 0; i < size-1; i++) {
+    //     min_index = i;
+    //     for (j = i + 1; j < size; j++) {
+    //       if (tempArray[j].taskPriority > tempArray[min_index].taskPriority) {
+    //         min_index = j
+    //       }
+    //     }
+    //     swap(tempArray, min_index, i);
+    //   }
+    // }
+
+    // function printArr(array) {
+    //   for (let i = 0; i < array.length; i++) {
+    //     console.log(array[i]);
+    //   }
+    // }
+
+    // function swap(tempArray, a, b) {
+    //   var temp = tempArray[a];
+    //   tempArray[a] = tempArray[b];
+    //   tempArray[b] = temp;
+    // }
+
+    // function sortByPriority() {
+    //   // let priorityArr = [];
+    //   // let temp = newTaskData;        
+
+    //   // selectionSort(temp, temp.length);
+    //   // printArr(temp);
+    //   // setNewTaskData([...temp]);
+      
+        
+    // } 
