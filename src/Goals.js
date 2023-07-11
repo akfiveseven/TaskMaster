0fcd21e0cccbf2ac08f970166b500c6c5faad79e
aymar <44442172+aymar1400@users.noreply.github.com>
2023-07-11 16:59:13 -0700

    fixed progress bar

--- src/Goals.js
+++ src/Goals.js
@@ -1,107 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
-import PropTypes from 'prop-types';
 import LinearProgress from '@mui/material/LinearProgress';
-import Typography from '@mui/material/Typography';
 
 export default function Goals({ goalData, newTaskData, checked }) {
+  const [goalDataState, setGoalData] = useState(goalData);
 
-  // const [progress, setProgress] = useState(0);
-  const [progressArray, setProgressArray] = useState([]);
+  useEffect(() => {
+    const updatedGoalData = goalData.map(goal => {
+      const goalTasks = newTaskData.filter(task => task.goalName === goal.goalName && task.taskType === "Goal");
 
-    useEffect(() => {
+      let numChecked = goalTasks.filter(task => checked.includes(task.taskID)).length;
+      let newProgress = 0;
+      if (goalTasks.length > 0) {
+        newProgress = (numChecked / goalTasks.length) * 100;
+      }
+      // Clone goal object and update progress
+      return {...goal, progress: newProgress};
+    });
 
+    setGoalData(updatedGoalData);
 
-
-      goalData.forEach(goal => {
-        const goalTasks = newTaskData.filter(task => {
-          if (task.goalName == goal.goalName) {
-            return true;
-          }
-          return false;
-        })
-
-        let numChecked = 0;
-
-        for (let i = 0; i < goalTasks.length; i++) {
-          if (checked.includes(goalTasks[i].taskID)) {
-            numChecked++;
-          }
-        }
-
-        console.log("Name: " + goal.goalName + " Progress: " + (Math.floor(numChecked / goalTasks.length) * 100));
-
-        // console.log("name: " + goal.goalName + " numCheck: " + numChecked);
-        // goal.progress = Math.floor((numChecked / goalTasks.length)) * 100;
-        // console.log("avg: " + goal.progress);
-
-
-      });
-
-
-      // const progress = totalTasks > 0 ? (totalChecked / totalTasks) * 100 : 0;
-
-
-
-
-
-    }, [goalData, newTaskData, checked]);  // dependencies
-
+  }, [goalData, newTaskData, checked]);  // dependencies
 
   return (
     <Box sx={{ height: 400, width: '100%' }}>
-
-      {goalData.map(goal =>  {
-        // const [progress, setProgress] = useState(0);
-
-
-
-        
-        const goalTasks = newTaskData.filter(task => {
-          if (task.goalName == goal.goalName) {
-            return true;
-          }
-          return false;
-        })
-
-        // let numChecked = 0;
-
-        // for (let i = 0; i < goalTasks.length; i++) {
-        //   if (checked.includes(goalTasks[i].taskID))
-        //     numChecked++;
-        // }
-        
-
-        // setProgress(100);
-
-        // if (isNaN(progress)) {
-        //   setProgress(0);
-        // }
-
+      {goalDataState.map(goal =>  {
+        const goalTasks = newTaskData.filter(task => task.goalName === goal.goalName);
         
         return (
           <>
-            {/*  GOAL TITLE */}
-            <h1>Check console log for progress</h1>
-            <h1 key={goal.id}>{goal.goalName}</h1>
-            {/* <h1>Size: {goalTasks.length}</h1>
-            <h1>Num Checked: {numChecked}</h1> */}
+            <h1 key={goal.id}>Goal: {goal.goalName}</h1>
             <h1>Progress: {goal.progress}%</h1>
             <Box sx={{ width: '100%' }}>
               <LinearProgress variant="determinate" value={goal.progress}/>
             </Box>
-
-
-
-            {goalTasks.map(GoalTask => {
-
-
-              return (
-                <>
-                  <h1 key={GoalTask.id}>{GoalTask.taskName}</h1>
-                </>
-              );
-            })}
+            {goalTasks.map(GoalTask => (
+              <ul>
+                <li key={GoalTask.id}>{GoalTask.taskName}</li>
+              </ul>
+            ))}
           </>
         ); // if the goal object has an `id` property
       })}
